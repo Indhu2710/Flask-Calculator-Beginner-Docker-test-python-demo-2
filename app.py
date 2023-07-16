19ab2405f4361e416be222f3219aad011603e90a
gitpradeephub <pradeeppearl67@gmail.com>
2023-07-16 14:42:13 +0530

    website updated

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ api.add_resource(Divide, "/division")
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return "I am interacting with u from pradeep server!"
 
 
 if __name__=="__main__":
